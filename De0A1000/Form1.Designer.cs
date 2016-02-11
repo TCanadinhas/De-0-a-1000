9990ec201c972048ec1a17d1d714fb108966cff1
Talita Canadinhas <talitacanadinhas@gmail.com>
2016-02-11 09:59:26 -0200

    Up project

---
+++ De0A1000/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace De0A1000
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.do_random = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 61);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 188);
+            this.textBox1.TabIndex = 0;
+            // 
+            // do_random
+            // 
+            this.do_random.Location = new System.Drawing.Point(92, 12);
+            this.do_random.Name = "do_random";
+            this.do_random.Size = new System.Drawing.Size(102, 30);
+            this.do_random.TabIndex = 1;
+            this.do_random.Text = "Do Random";
+            this.do_random.UseVisualStyleBackColor = true;
+            this.do_random.Click += new System.EventHandler(this.do_random_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.do_random);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button do_random;
+
+    }
+}
+
---
+++ De0A1000/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace De0A1000
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random random = new Random();
+
+ 
+        public int[] results = new int[50];
+        public int min = 0;
+        public int max = 51;
+        int element;
+
+        public void doRandom()
+        {
+            for (int i=0; i<results.Length; i++)
+            {
+                element = random.Next(min, max);
+                while(results.Contains(element))
+                {
+                    element = random.Next(min, max);
+                }
+                results[i] = element;
+            }
+            Array.Sort(results);
+        }
+
+        private void do_random_Click(object sender, EventArgs e)
+        {
+            doRandom();
+            for(int i=0; i<50; i++)
+            {
+                textBox1.Text += results[i].ToString() + " | ";
+            }
+        }
+   
+        
+
+    }
+}
